3a28c3fd01214307978b1a80d2824c8da579d7ca
Eric Stevens <eric@ericstevens.ca>
2023-04-17 16:39:09 -0400

    add extract

--- makeIconJson.py
+++ makeIconJson.py
@@ -1,11 +1,31 @@
+import json
 
-
+def extractIcon(l:str)->dict:
+    # -a----        2023-04-16   5:40 PM           1763 gauss-pistol.svg
+    lineOut = {}
+    indexLastSpace = l.rindex(' ')
+    imgName = l[indexLastSpace:]
+    lineOut['img'] = imgName
+    rawName = imgName[:-4]
+    if rawName.find('-') != -1:
+        nameElements = rawName.split('-')
+    else:
+        if rawName.find('_') != -1:
+            nameElements = rawName.split('_')
+        else:
+            nameElements.append(rawName)
+    lineOut['tags'] = nameElements
+    return lineOut
 
 def main ():
     iconFileName = 'icons.txt'
+    iconOutput = {"icons": []}
+    iconOutputfile = 'icon.json'
     with open(iconFileName, "r") as iconFD:
         line = iconFD.read()
-        print(line)
+        iconOutput['icons'].append(extractIcon(line))
+    with open(iconOutputfile, 'w') as iconOutputFD:
+        iconOutputFD.write(json.dump(iconOutput, ensure_ascii=True, indent=4))
     return True
 
 if __name__ == "__main__":
